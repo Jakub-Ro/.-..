9df81721177b2a64f46851a3aed486333c2f57e1
Jakub <144731224+Jakub-Ro@users.noreply.github.com>
2023-10-27 07:05:12 +0200

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+﻿h = int(input("Czas startu (godziny): "))
+m = int(input("Czas startu (minuty): "))
+d = int(input("Czas trwania wydarzenia (minuty): "))
+
+m = m + d
+
+h = h + m // 60
+
+print(h % 24, ":", m % 60, sep="")
